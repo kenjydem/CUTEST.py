0e284783502528317a0b261e2f04841db4765956
Kenjy Demeester <kenjy.dem@gmail.com>
2016-02-19 10:13:19 -0500

    updated

--- cutest/projet_Cython.py
+++ cutest/projet_Cython.py
@@ -22,9 +22,9 @@ def sifdecoder(name):
   pname, sif = name.split('.')
   libname = 'lib'+ pname
   subprocess.call(['sifdecoder', name])
-  os.system('gfortran -c -fPIC ELFUN.f EXTER.f GROUP.f RANGE.f')
-  os.system(linker+" "+ sh_flags +" -o "+libname+"."+soname+" ELFUN.o EXTER.o GROUP.o RANGE.o -L"+ CUTEST_DIR+os.path.sep+"objects"+os.path.sep+CUTEST_ARCH+os.path.sep+"double")
-  os.system('rm ELFUN.f EXTER.f GROUP.f RANGE.f ELFUN.o EXTER.o GROUP.o RANGE.o')
+  subprocess.call('gfortran -c -fPIC ELFUN.f EXTER.f GROUP.f RANGE.f',shell=True)
+  subprocess.call([linker,sh_flags,"-o",libname+"."+ soname,"ELFUN.o","EXTER.o","GROUP.o","RANGE.o","-L"+ CUTEST_DIR+os.path.sep+"objects"+os.path.sep+CUTEST_ARCH+os.path.sep+"double"])
+  subprocess.call('rm ELFUN.f EXTER.f GROUP.f RANGE.f ELFUN.o EXTER.o GROUP.o RANGE.o',shell=True)
   return libname
 
 
